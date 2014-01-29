1ffd852d36b1e75bbaacb561b116ca324ae8f9e5
Adam Malcontenti-Wilson <adman.com@gmail.com>
2014-01-29 17:42:24 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Castme::Application.config.secret_key_base = '1470c182656b51bfaccbd16d24fa491dc6b6bf8b9d6ee1acac94cec0a398ae30f960717be3adffd6fafc999325341db87e744e983f555060208fe39a4f217a06'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Castme::Application.config.session_store :cookie_store, key: '_castme_session'
